25d913ac90a90a287776ef8a93c6031d05a4a397
emaiannone <emaiannone96@gmail.com>
2021-01-29 12:48:30 +0100

    Use convenience method in ProjectAnalyzerResults to spare useless statements

--- src/main/java/org/name/tool/core/metrics/projectlevel/cme/CMEImpl.java
+++ src/main/java/org/name/tool/core/metrics/projectlevel/cme/CMEImpl.java
@@ -1,21 +1,16 @@
 package org.name.tool.core.metrics.projectlevel.cme;
 
 import com.github.javaparser.ast.body.MethodDeclaration;
-import org.name.tool.core.results.ClassifiedAnalyzerResults;
 import org.name.tool.core.results.ProjectAnalyzerResults;
 import org.name.tool.core.results.SecurityMetricResult;
 
-import java.util.HashSet;
 import java.util.Set;
 
 public class CMEImpl extends CME {
 
     @Override
     public SecurityMetricResult<Double> compute(ProjectAnalyzerResults projectResults) {
-        Set<MethodDeclaration> allClassifiedMethods = new HashSet<>();
-        for (ClassifiedAnalyzerResults classResults : projectResults) {
-            allClassifiedMethods.addAll(new HashSet<>(classResults.getClassifiedMethods()));
-        }
+        Set<MethodDeclaration> allClassifiedMethods = projectResults.getClassifiedMethods();
         int totalCM = allClassifiedMethods.size();
         long nonFinalCM = allClassifiedMethods.stream().filter(md -> !md.isFinal()).count();
         double value = totalCM != 0.0 ? (double) nonFinalCM / totalCM : 0.0;
