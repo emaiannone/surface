5c90557b09c4e6a84e240c13eb4c922b0cb97288
emaiannone <emaiannone96@gmail.com>
2021-01-29 22:12:10 +0100

    Improve reuse in CC metric

--- src/main/java/org/name/tool/core/metrics/projectlevel/cc/CCImpl.java
+++ src/main/java/org/name/tool/core/metrics/projectlevel/cc/CCImpl.java
@@ -1,6 +1,5 @@
 package org.name.tool.core.metrics.projectlevel.cc;
 
-import org.name.tool.core.results.ClassifiedAnalyzerResults;
 import org.name.tool.core.results.MetricResult;
 import org.name.tool.core.results.ProjectAnalyzerResults;
 
@@ -8,12 +7,7 @@ public class CCImpl extends CC {
 
     @Override
     public MetricResult<Integer> compute(ProjectAnalyzerResults projectResults) {
-        int value = 0;
-        for (ClassifiedAnalyzerResults classResults : projectResults) {
-            if (classResults.isCritical()) {
-                value++;
-            }
-        }
+        int value = projectResults.getCriticalClasses().size();
         return new MetricResult<>(getName(), getCode(), value);
     }
 }
