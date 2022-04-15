f12533ef974019e42c8ee1ba57c82b3daad40c75
emaiannone <emaiannone96@gmail.com>
2022-04-15 17:17:22 +0200

    Replace absolute path with getResourceAsStream()

--- src/main/java/org/surface/surface/core/analysis/ClassifiedPatterns.java
+++ src/main/java/org/surface/surface/core/analysis/ClassifiedPatterns.java
@@ -1,7 +1,7 @@
 package org.surface.surface.core.analysis;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,20 +9,24 @@ import java.util.regex.Pattern;
 
 public class ClassifiedPatterns {
     private static ClassifiedPatterns INSTANCE;
-    private final String patternsPath = "src/main/resources/patterns";
     private List<Pattern> patterns;
 
     private ClassifiedPatterns() {
         try {
-            Scanner scanner = new Scanner(new File(patternsPath));
-            patterns = new ArrayList<>();
-            while (scanner.hasNext()) {
-                String line = scanner.nextLine();
-                String regex = ".*" + line + "[^\\s]*";
-                Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
-                patterns.add(pattern);
+            try (InputStream patternsIS = getClass().getClassLoader().getResourceAsStream("patterns")) {
+                if (patternsIS == null) {
+                    throw new IOException();
+                }
+                Scanner scanner = new Scanner(patternsIS);
+                patterns = new ArrayList<>();
+                while (scanner.hasNext()) {
+                    String line = scanner.nextLine();
+                    String regex = ".*" + line + "[^\\s]*";
+                    Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+                    patterns.add(pattern);
+                }
             }
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
